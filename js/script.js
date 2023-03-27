c20e30b2fae9cb8ed796aa2720840f2eb0fac18f
Pixel422 <abben1000@gmail.com>
2023-03-28 00:06:32 +0200

    Fixed full layout
    Still missing server/database as well as tags for the images.

--- js/script.js
+++ js/script.js
@@ -10,22 +10,33 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     navMenu.classList.remove("active");
 }));
 
-// const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-// document.querySelector(".nav-link").onmouseover = event => {
-//     let iterations = 0;
+const imageInput = document.querySelector("#image-input");
+var uploadedImage = "";
 
-//     const interval = setInterval(() => {
-//     event.target.innerText = event.target.innerText.split("").map((letter, index) => {
-//         if(index < iterations){
-//             return event.target.dataset.value[index];
-//         }
-        
-//         return letters[Math.floor(Math.random() * 26)]
-//     })
-//         .join("");
+imageInput.addEventListener("change", function() {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+        uploadedImage = reader.result;
+        document.querySelector(".card").style.backgroundImage = `url(${uploadedImage})`;
+    });
+    reader.readAsDataURL(this.files[0]);
+});
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+document.querySelector(".upload-label").onmouseover = event => {
+    let iterations = 0;
+
+    const interval = setInterval(() => {
+    event.target.innerText = event.target.innerText.split("")
+    .map((letter, index) => {
+    if(index < iterations) {
+        return event.target.dataset.value[index];
+    }
     
-//     if(iterations === event.target.dataset.value.length) clearInterval(interval);
-
-//     iterations += 1 / 4;
-//     }, 30);
-// }
+    return letters[Math.floor(Math.random() * 26)]
+    })
+    .join("");
+    if (iterations === event.target.dataset.value.length) clearInterval(interval);
+    iterations += 1 / 2;
+}, 30);
+}
